e2cd84a91de539bebf62c005a4aa16636834be01
Stephani McGrath <silentsoundz1977@gmail.com>
2017-09-25 10:35:12 -0700

    creates git repo

--- queries.js
+++ queries.js
@@ -1,17 +1,14 @@
 const pgp = require('pg-promise')()
-const db = pgp(connectionString);
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/flashcards';
+const db = pgp(connectionString);
+
+function getCards(category_id) {
+  return db.any('SELECT * FROM cards WHERE category_id = $1', [category_id])
+}
 
 function getAllCards() {
   return db.any('SELECT * FROM cards')
 }
 
-function getJavaScriptCards() {
-  return db.any('SELECT * FROM cards WHERE category_id = 1')
-}
 
-function getSqlCards() {
-  return db.any('SELECT * FROM cards WHERE category_id = 2')
-}
-
-module.exports = { getAllCards, getJavaScriptCards, getSqlCards }
+module.exports = { getAllCards, getCards }
--- routes/cards.js
+++ routes/cards.js
@@ -1,2 +1,34 @@
-/*
-this will be routes for the question and answer 
+const router = require('express').Router()
+const { getAllCards, getCards } = require('../queries')
+
+
+router.get('/', (request, response) => {
+  console.log("index page");
+  response.render('index')
+})
+
+router.get('/cards/category/:category_id', (request, response) => {
+  const category_id = request.params.category_id
+  console.log(category_id);
+  getCards(category_id)
+    .then(cards => {
+      response.status(200)
+      response.render('play', cards)
+    })
+    .catch(error => {
+      response.status(500)
+    })
+})
+
+
+
+router.get('/cards/category/wildcardFlashcards', (request, response) => {
+  response.render('wildcardFlashcards')
+})
+
+
+
+
+module.exports = router;
+
+
